34e27e42db001146f4e4b5d603ebd829c65bc1bb
Divyank Mishra <divyank.mishra@apnamart.in>
2024-12-18 16:45:58 +0530

    Change name in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="py-redis-client",
+    name="py_redis_client",
     version="1.0.0.0",
     packages=find_packages(),
     include_package_data=True,

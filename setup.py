57333a9e183c023d4061a18ba67e01df521267ba
Divyank Mishra <divyank.mishra@apnamart.in>
2024-12-18 15:42:47 +0530

    Remove redis dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-       "redis==5.0.3", "django>=4.2"],
+       "redis", "django>=4.2"],
     description="A helper library, built over redis-py, to use as cache, lock etc.",
     license='MIT',
     long_description=open('README.md').read(),
